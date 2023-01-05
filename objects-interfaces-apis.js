4ec354bebe1a0706809978793cb7ef6ac4049057
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-01-05 15:08:53 -0700

    completion of API video

--- objects-interfaces-apis.js
+++ objects-interfaces-apis.js
@@ -1,8 +1,8 @@
 /*
 *Objects, Interfaces, and API's
  */
-
-/* let dog = {
+/*
+let dog = {
     name: "Steve",
     color: "brown",
     breed: "Pug",
@@ -12,6 +12,17 @@
     },
 };
 
-//dog.bark()
-*/
+dog.bark()
+ */
 
+function x(y) {
+    y.num = y.num + 5;
+    console.log(y);
+}
+
+let y = {
+    name: "Tom",
+    num: 10,
+};
+x(y);
+console.log (y)
